0108a74e5da5985cccf7fe9fba780e6627d045da
Mishaal Khalid <m_khalid2@uncg.edu>
2024-10-25 22:21:30 -0400

    Set up MVC structure with Thymeleaf templates

--- src/main/java/com/csc340/Assignment04/AnimalController.java
+++ src/main/java/com/csc340/Assignment04/AnimalController.java
@@ -2,16 +2,24 @@ package com.csc340.Assignment04;
 
 //import com.csc340.Assignment04.entity.Animal;
 //import com.csc340.Assignment04.animal.AnimalController;
+import ch.qos.logback.core.model.Model;
 import com.csc340.Assignment04.animal.Animal;
 import com.csc340.Assignment04.AnimalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("animal")
 public class AnimalController {
+    @GetMapping("/animals")
+    public String getAllAnimals(Model model) {
+        List<Animal> animals = service.getAllAnimals();
+        model.addAttribute("animalList", animals);
+        return "animal-list"; // Returns the name of the HTML view
+    }
 
     @Autowired
     private AnimalService service;
